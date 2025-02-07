072d391f3197bf697a85008200e066b415ceef3c
Caleb Roskelley <caleb.roskelley@students.snow.edu>
2025-02-06 19:22:12 -0700

    fixed some issues with code

--- Program.cs
+++ Program.cs
@@ -8,6 +8,66 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        List<Task> TaskList = new List<Task>();
+        Console.WriteLine("Hello, this program will allow you to input things you need to do, and you can check them off as you go!");
+        Console.WriteLine("Press \"+\" if you would like to add to the list, \"i\" if you'd like information on that task and \"x\" to mark is as completed");
+        bool keepGoing = true;
+        while(keepGoing)
+        {
+            Console.WriteLine("What would you like to do?");
+            string userChoice = Console.ReadLine();
+            switch (userChoice)
+            {
+                case "+":
+                    Task userToDo = new Task();
+                    userToDo.ID = TaskList.Count();
+                    userToDo.Description = addDescription();
+                    userToDo.Title = setTitle();
+                    break;
+                case "i":
+                    // userToDo.ID = whichTask() - 1;
+                    // userToDo.Title = TaskList[userToDo.ID];
+                    // userToDo.Description = TaskDescription[userToDo.ID];
+                    // userToDo.DisplayDescription();
+                    TaskList[whichTask(TaskList.Count())-1].DisplayDescription();
+                    break;
+                case "x":
+                    whichTask(TaskList.Count());
+                    break;
+                    
+            }
+            foreach(var item in TaskList)
+            {
+                item.DisplayTask();
+            }
+        }
+    }
+    public static string setTitle()
+    {
+        Console.WriteLine("Please provide the title of your task");
+        return Console.ReadLine();
+    }
+
+    public static string addDescription()
+    {
+        Console.WriteLine("Please provide a description for this task");
+        return Console.ReadLine();
+    }
+
+    public static int whichTask(int count)
+    {
+        bool isValid = false;
+        int num;
+        do
+        {
+            Console.WriteLine("Which task");
+            isValid = int.TryParse(Console.ReadLine(), out num);
+            if(num < 1 || num > count)
+            {
+                isValid = false;
+            }
+        }
+        while(!isValid);
+        return num;
     }
 }
--- Task.cs
+++ Task.cs
@@ -1,10 +1,27 @@
-class Task
+public class Task
 {
-    public int? ID {get; set;}
-    public string? Title {get; set;}
-    public string? Description {get; set;}
+    public int ID {get; set;}
+    public string Title {get; set;}
+    public string Description {get; set;}
     public bool isCompleted {get; set;}
 
+    public Task(int id, string title, string description)
+    {
+        ID = id;
+        Title = title;
+        Description = description;
+    }
+    public Task(int id, string title, string description, bool _isCompleted)
+    {
+        ID = id;
+        Title = title;
+        Description = description;
+        isCompleted = _isCompleted;
+    }
+
+    public Task()
+    {
+    }
 
     public void DisplayTask()
     {
